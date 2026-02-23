76372735f405268f5e65d0632c4dbf95a32c07a1
Aaron Cowdrey <aaron.cowdrey@dynamicautosol.com>
2026-02-23 14:17:28 -0600

    Add MUI Boilerplate and Prettier

---
+++ src/components/Link.tsx
@@ -0,0 +1,5 @@
+'use client'
+// re-export as client component, see here https://mui.com/material-ui/integrations/nextjs/#next-js-v16-client-component-restriction
+import Link, { LinkProps } from 'next/link'
+
+export default Link
